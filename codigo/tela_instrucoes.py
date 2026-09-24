b0d11cb14bf2f7c04f8313d17ea27f5148b318d1
Julia Vexel <jucvfontes@gmail.com>
2026-09-24 00:29:58 -0300

    Alterações de instruções

--- codigo/tela_instrucoes.py
+++ codigo/tela_instrucoes.py
@@ -7,25 +7,25 @@ def desenha_tela(janela, estado, altura, largura):
     motor.desenha_string(janela, (largura-(len('instrucoes')))//2, 3, 'INSTRUÇÕES', AZUL_CLARO, AMARELO_DOURADO)
 
     motor.desenha_string(janela, 5, 6, 'COMANDOS:', AZUL_CLARO, BRANCO)
-    motor.desenha_string(janela, 5, 7, '1: Aperte a tecla i para acessar seu inventário e retornar à tela do jogo', AZUL_CLARO, BRANCO)
-    motor.desenha_string(janela, 5, 8, '2: Dentro da sala secreta, aperte espaço para retornar à tela do jogo', AZUL_CLARO, BRANCO)
-    motor.desenha_string(janela, 5, 9, '3: Aperte esc ou a tecla q para sair do jogo', AZUL_CLARO, BRANCO)
-    motor.desenha_string(janela, 5, 10, '4: Use as setas para movimentar o personagem', AZUL_CLARO, BRANCO)
-    motor.desenha_string(janela, 5, 11, '5: Aperte ↓ para voltar ao menu inicial', AZUL_CLARO, BRANCO)
+    motor.desenha_string(janela, 5, 7, 'Aperte a tecla i para acessar seu inventário e retornar à tela do jogo', AZUL_CLARO, BRANCO)
+    motor.desenha_string(janela, 5, 8, 'Dentro da sala secreta, aperte espaço para retornar à tela do jogo', AZUL_CLARO, BRANCO)
+    motor.desenha_string(janela, 5, 9, 'Aperte esc ou a tecla q para sair do jogo', AZUL_CLARO, BRANCO)
+    motor.desenha_string(janela, 5, 10, 'Use as setas para movimentar o personagem', AZUL_CLARO, BRANCO)
+    motor.desenha_string(janela, 5, 11, 'Aperte ↓ para voltar ao menu inicial', AZUL_CLARO, BRANCO)
 
     motor.desenha_string(janela, 5, 13, 'MONSTROS:', AZUL_CLARO, BRANCO)
-    motor.desenha_string(janela, 5, 14, '1: Batalhe com os monstros para ganhar experiência e subir de nível', AZUL_CLARO, BRANCO)
-    motor.desenha_string(janela, 5, 15, '2: Monstros do tipo ☠  possuem poder de ataque de 0.70 e 2 vidas', AZUL_CLARO, BRANCO)
-    motor.desenha_string(janela, 5, 16, '3: Monstros do tipo ♣ possuem poder de ataque de 0.30 e 5 vidas', AZUL_CLARO, BRANCO)
-    motor.desenha_string(janela, 5, 17, '4: Monstros do tipo Ψ possuem poder de ataque de 0.45 e 3 vidas', AZUL_CLARO, BRANCO)
+    motor.desenha_string(janela, 5, 14, 'Batalhe com os monstros para ganhar experiência e subir de nível', AZUL_CLARO, BRANCO)
+    motor.desenha_string(janela, 5, 15, 'Monstros do tipo ☠  possuem poder de ataque de 0.70 e 2 vidas', AZUL_CLARO, BRANCO)
+    motor.desenha_string(janela, 5, 16, 'Monstros do tipo ♣ possuem poder de ataque de 0.30 e 5 vidas', AZUL_CLARO, BRANCO)
+    motor.desenha_string(janela, 5, 17, 'Monstros do tipo Ψ possuem poder de ataque de 0.45 e 3 vidas', AZUL_CLARO, BRANCO)
     
     motor.desenha_string(janela, 5, 19, 'ITENS:', AZUL_CLARO, BRANCO)
-    motor.desenha_string(janela, 5, 20, '1: Com a espada equipada, você fica mais forte contra os monstros', AZUL_CLARO, BRANCO)
-    motor.desenha_string(janela, 5, 21, '2: Com o martelo equipado, monstros têm sua vida reduzida', AZUL_CLARO, BRANCO)
-    motor.desenha_string(janela, 5, 22, '3: Poções podem adicionar ou tirar vidas do seu total', AZUL_CLARO, BRANCO)
-    motor.desenha_string(janela, 5, 23, '4: O elixir te fornece pontos de experiência', AZUL_CLARO, BRANCO)
-    motor.desenha_string(janela, 5, 24, '5: Colete 3 chaves e libere a sala secreta', AZUL_CLARO, BRANCO)
-        
+    motor.desenha_string(janela, 5, 20, f'Com a espada equipada, você fica mais forte contra os monstros: aperte {'s'}', AZUL_CLARO, BRANCO)
+    motor.desenha_string(janela, 5, 21, f'Com o martelo equipado, monstros têm sua vida reduzida: aperte {'h'}', AZUL_CLARO, BRANCO)
+    motor.desenha_string(janela, 5, 22, f'Poções podem adicionar ou tirar vidas do seu total: aperte {'p'}', AZUL_CLARO, BRANCO)
+    motor.desenha_string(janela, 5, 23, f'O elixir te fornece pontos de experiência: aperte {'e'}', AZUL_CLARO, BRANCO)
+    motor.desenha_string(janela, 5, 24, f'Colete 3 chaves e libere a sala secreta: aperte {'k'}', AZUL_CLARO, BRANCO)
+    
 
     motor.desenha_string(janela, (largura-(len('divirta-se!')))//2, altura-4, 'DIVIRTA-SE!', AZUL_CLARO, BRANCO)
     
